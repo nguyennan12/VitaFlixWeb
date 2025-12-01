1c111baccd7e8b02e2c2c43b9f7417fec848f611
Nguyen An <nannan@example.com>
2025-12-02 00:35:31 +0700

    update function get data by slug

---
+++ js/modules/utils.js
@@ -0,0 +1,27 @@
+import { MovieDetail } from "./model.js";
+
+const DETAIL_BASE_URL = 'https://phimapi.com/phim/';
+
+export const getMovieBySlug = async (slug) => {
+    try {
+        // Gọi API lấy chi tiết phim
+        const response = await axios.get(`${DETAIL_BASE_URL}${slug}`);
+        
+        if (response.data) {
+            
+            const movieDetail = new MovieDetail(response.data);
+            
+            return movieDetail;
+        }
+        
+        return null;
+    } catch (error) {
+        console.error(`Lỗi khi tải phim với slug "${slug}":`, error);
+        return null;
+    }
+};
+
+
+
+const movie = await getMovieBySlug('nguu-lang-chuc-nu');
+console.log(movie);
